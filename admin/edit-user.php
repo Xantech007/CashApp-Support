8db2c8d2c6d67edde78d21754dd063f8a7dd0a5e
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2025-07-24 07:41:25 +0200

    Update edit-user.php

--- admin/edit-user.php
+++ admin/edit-user.php
@@ -5,15 +5,10 @@ include('inc/navbar.php');
 include('inc/sidebar.php');
 ?>
 
- 
-
-  <!-- ======= Sidebar ======= -->
-
-
-  <main id="main" class="main">
+<main id="main" class="main">
  
   <div class="pagetitle">
-      <h1>Edit User  Detials</h1>
+      <h1>Edit User Details</h1>
       <nav>
         <ol class="breadcrumb">
           <li class="breadcrumb-item"><a href="dashbaord">Home</a></li>
@@ -31,11 +26,10 @@ include('inc/sidebar.php');
         }
     </style>
         
-
     <div class="container">
         <div class="row">
-            <div class="card"style="padding:10px">
-            <form action="codes/users.php"method="POST">
+            <div class="card" style="padding:10px">
+            <form action="codes/users.php" method="POST">
                 <?php
                 if(isset($_GET['id']))
                 {
@@ -51,7 +45,6 @@ include('inc/sidebar.php');
                         $id = $row['id'];                       
                         $balance = $row['balance'];                       
                         $email = $row['email'];                       
-                        $wallet = $row['btc_wallet'];                       
                         $referral = $row['refered_by'];                       
                         $country = $row['country'];                       
                         $bonus = $row['referal_bonus'];                                         
@@ -60,60 +53,42 @@ include('inc/sidebar.php');
                 ?>
                 <div class="row">
                 <div class="col-md-6 form-group mb-3">
-                    <label for=""class="mb-2">Package name</label>
-                      <input type="text" class="form-control"required value="<?= $name ?>"readonly>
-                    </div>                                  
+                    <label for="" class="mb-2">Package name</label>
+                    <input type="text" class="form-control" required value="<?= $name ?>" readonly>
+                </div>                                  
                 <div class="col-md-6 form-group mb-3">
-                    <label for=""class="mb-2">Email</label>
-                      <input name="email" type="text" class="form-control"required value="<?= $email ?>">
-                    </div>                                  
-                    <div class="col-md-6 form-group mb-3">
-                    <label for=""class="mb-2">Country</label>
-                      <input type="text" class="form-control"required  value="<?= $country ?>"readonly>
-                    </div> 
-                    <div class="col-md-6 form-group mb-3">
-                    <label for=""class="mb-2">Bitcoin Wallet</label>
-                      <input type="text" class="form-control"required  value="<?= $wallet ?>"readonly>
-                    </div> 
-                    <div class="col-md-6 form-group mb-3">
-                    <label for=""class="mb-2">Balance</label>
-                      <input name="balance" type="number" class="form-control"required  value="<?= $balance ?>">
-                    </div> 
-                    <div class="col-md-6 form-group mb-3">
-                    <label for=""class="mb-2">Referal Bonus</label>
-                      <input name="referal_bonus" type="number" class="form-control"required  value="<?= $bonus ?>">
-                    </div> 
+                    <label for="" class="mb-2">Email</label>
+                    <input name="email" type="text" class="form-control" required value="<?= $email ?>">
+                </div>                                  
+                <div class="col-md-6 form-group mb-3">
+                    <label for="" class="mb-2">Country</label>
+                    <input type="text" class="form-control" required value="<?= $country ?>" readonly>
+                </div> 
+                <div class="col-md-6 form-group mb-3">
+                    <label for="" class="mb-2">Balance</label>
+                    <input name="balance" type="number" class="form-control" required value="<?= $balance ?>">
+                </div> 
+                <div class="col-md-6 form-group mb-3">
+                    <label for="" class="mb-2">Referral Bonus</label>
+                    <input name="referal_bonus" type="number" class="form-control" required value="<?= $bonus ?>">
+                </div> 
                                      
-                  
-                    <button type="submit" class="btn btn-secondary"name="update_user"value=<?= $id ?>>Update</button>  
+                <button type="submit" class="btn btn-secondary" name="update_user" value="<?= $id ?>">Update</button>  
                 </div>  
-                </form>
-                    <style>
-                        input[type=number]::-webkit-inner-spin-button,
-                        input[type=number]::-webkit-outer-spin-button {
-                        -webkit-appearance: none;
-                        margin: 0;
-        }
-                    </style>
+            </form>
+            <style>
+                input[type=number]::-webkit-inner-spin-button,
+                input[type=number]::-webkit-outer-spin-button {
+                    -webkit-appearance: none;
+                    margin: 0;
+                }
+            </style>
         </div>    
-   </div>       
-        <div class="add-btn">
-           <a href="manage-users" class="btn btn-secondary">Back</a>
-        </div>
-              
-       
-
-       
- 
-  
-            
-
-        
-   
-
-    
-
-  </main><!-- End #main -->
+    </div>       
+    <div class="add-btn">
+        <a href="manage-users" class="btn btn-secondary">Back</a>
+    </div>
+</main><!-- End #main -->
  
 <?php include('inc/footer.php'); ?>
 
