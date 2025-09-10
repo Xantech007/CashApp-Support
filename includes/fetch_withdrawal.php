1ba223161a3200c452d2b36d94127a7fc654afc7
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2025-09-10 04:40:04 +0000

    Create fetch_withdrawal.php

---
+++ includes/fetch_withdrawal.php
@@ -0,0 +1,19 @@
+<?php
+include '../dbconfig.php'; // Your database connection file
+
+$query = "SELECT name, amount FROM withdrawals ORDER BY RAND() LIMIT 1";
+$result = mysqli_query($con, $query);
+
+if ($row = mysqli_fetch_assoc($result)) {
+    header('Content-Type: application/json');
+    echo json_encode([
+        'name' => $row['name'],
+        'amount' => number_format($row['amount'], 2)
+    ]);
+} else {
+    header('Content-Type: application/json');
+    echo json_encode([]);
+}
+
+mysqli_close($con);
+?>
