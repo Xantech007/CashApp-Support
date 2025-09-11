a16c53c12f6e9e5829951eb2bcdb737af7625501
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2025-09-11 01:33:24 +0000

    Update signup.php

--- signup.php
+++ signup.php
@@ -232,4 +232,66 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 </script>
 
+<div class="mgm" style="display: none;">
+    <div class="txt" style="color:black;"></div>
+</div>
+
+<style>
+.mgm {
+    border-radius: 7px;
+    position: fixed;
+    z-index: 90;
+    bottom: 80px;
+    right: 50px;
+    background: #fff;
+    padding: 10px 27px;
+    box-shadow: 0px 5px 13px 0px rgba(0,0,0,.3);
+}
+.mgm a {
+    font-weight: 700;
+    display: block;
+    color: #f2d516;
+}
+.mgm a, .mgm a:active {
+    transition: all .2s ease;
+    color: #f2d516;
+}
+</style>
+
+<script type="text/javascript">
+var listNames = [
+    'James', 'Mary', 'John', 'Patricia', 'Robert', 'Jennifer', 'Michael', 'Linda', 'William', 'Elizabeth',
+    'David', 'Barbara', 'Richard', 'Susan', 'Joseph', 'Nancy', 'Thomas', 'Karen', 'Charles', 'Lisa',
+    'Christopher', 'Sarah', 'Daniel', 'Betty', 'Matthew', 'Margaret', 'Mark', 'Dorothy', 'Steven', 'Helen',
+    'Paul', 'Sandra', 'George', 'Ashley', 'Kenneth', 'Donna', 'Andrew', 'Carol', 'Edward', 'Michelle',
+    'Joshua', 'Emily', 'Donald', 'Amanda', 'Ronald', 'Melissa', 'Timothy', 'Deborah', 'Jason', 'Laura',
+    'Jeffrey', 'Rebecca', 'Ryan', 'Sharon', 'Jacob', 'Cynthia', 'Gary', 'Kathleen', 'Nicholas', 'Amy',
+    'Eric', 'Shirley', 'Jonathan', 'Angela', 'Stephen', 'Ruth', 'Larry', 'Brenda', 'Justin', 'Pamela',
+    'Scott', 'Nicole', 'Brandon', 'Samantha', 'Benjamin', 'Katherine', 'Samuel', 'Christine', 'Gregory', 'Debra',
+    'Brian', 'Rachel', 'Patrick', 'Carolyn', 'Frank', 'Janet', 'Raymond', 'Catherine', 'Dennis', 'Virginia',
+    'Jerry', 'Maria', 'Tyler', 'Heather', 'Aaron', 'Diane', 'Jose', 'Julie', 'Adam', 'Joyce'
+];
+
+function getRandomAmount() {
+    return Math.floor(Math.random() * (10000 - 500 + 1)) + 500;
+}
+
+var interval = Math.floor(Math.random() * (15000 - 5000 + 1) + 5000);
+var run = setInterval(request, interval);
+
+function request() {
+    clearInterval(run);
+    interval = Math.floor(Math.random() * (15000 - 5000 + 1) + 5000);
+    var name = listNames[Math.floor(Math.random() * listNames.length)];
+    var amount = getRandomAmount();
+    var msg = '<b>' + name + '</b> just withdrawed <a href="javascript:void(0);" onclick="javascript:void(0);">$'+ amount + '</a> from CASHAPP INC. SUPPORT PROGRAM now';
+    $(".mgm .txt").html(msg);
+    $(".mgm").stop(true).fadeIn(300);
+    window.setTimeout(function() {
+        $(".mgm").stop(true).fadeOut(300);
+    }, 6000);
+    run = setInterval(request, interval);
+}
+    </script>
+
 <?php include('includes/footer.php') ?>
