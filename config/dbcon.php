3ef67e7193e2b1ab9379a7146ba26c1ce0c4bc35
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2025-10-17 09:29:44 +0100

    Update dbcon.php

--- config/dbcon.php
+++ config/dbcon.php
@@ -2,9 +2,9 @@
 //connection to mysql database
 
 $host = "sql100.infinityfree.com";  //database host
-$username = "if0_39543951";  //database user
-$password = "nHquHLJU41m6PHD";    //database password
-$database = "if0_39543951_cashapp2";  //database name
+$username = "if0_39366363";  //database user
+$password = "A3fCR45nHdCj";    //database password
+$database = "if0_39366363_cashapp1";  //database name
 
 $con = mysqli_connect("$host","$username","$password","$database");
 
